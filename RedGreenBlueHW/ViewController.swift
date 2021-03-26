f24ad34f597030e81371635a5c3de93fab94edcd
SanShamor <sanshamor@mail.ru>
2021-03-27 00:49:54 +0300

    Need some polishing

--- RedGreenBlueHW/ViewController.swift
+++ RedGreenBlueHW/ViewController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    @IBOutlet weak var buttonVision: UIButton!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonVision.isHidden = true
+        redValueLabel.isHidden = true
+        greenValueLabel.isHidden = true
+        blueValueLabel.isHidden = true
+        
+        redSlider.value = 0.0
+        greenSlider.value = 0.0
+        blueSlider.value = 0.0
+        
+        viewColor.layer.cornerRadius = 45
+        viewColor.layer.borderColor = UIColor.black.cgColor
+        viewColor.layer.borderWidth = 2
     }
 
-
+    @IBAction func slidersValue(_ sender: UISlider) {
+        updateColor()
+    }
+    
+    @IBAction func buttonPressedClear(_ sender: UIButton) {
+        viewColor.backgroundColor = UIColor.clear
+        redSlider.value = 0.0
+        greenSlider.value = 0.0
+        blueSlider.value = 0.0
+    }
+    
+    
+    
+    private func updateColor() {
+        buttonVision.isHidden = false
+        redValueLabel.isHidden = false
+        greenValueLabel.isHidden = false
+        blueValueLabel.isHidden = false
+        
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        red = CGFloat(redSlider.value)
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        green = CGFloat(greenSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blue = CGFloat(blueSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        viewColor.backgroundColor = color
 }
 
+}
